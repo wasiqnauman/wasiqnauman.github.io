a26ded40c2b0d77fbe9f5b1e4ae0b052c58608f6
Wasiq Nauman <46854925+wasiqnauman@users.noreply.github.com>
2026-02-16 11:14:49 -0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export', // Required for GitHub Pages
+  images: {
+    unoptimized: true, // GitHub Pages doesn't support the default Next.js Image Optimization
+  },
 };
 
 export default nextConfig;
